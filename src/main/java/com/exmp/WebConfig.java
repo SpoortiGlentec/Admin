fe5297ca2c5ff4947535e8e1997c8a4f5062bb97
SpoortiGlentec <Spoorti.Angadi@glentec-digital.com>
2025-04-08 10:54:42 +0530

    Add files via upload

---
+++ src/main/java/com/exmp/WebConfig.java
@@ -0,0 +1,17 @@
+package com.exmp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/images/**")
+            .addResourceLocations("file:///D:/Images/"); // change path as per your IMAGE_FOLDER_PATH
+    }
+}
+
